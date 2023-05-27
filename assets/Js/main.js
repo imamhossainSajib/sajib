8bdc4f2e749e8d824eb56a5ed14647528d2c9c9f
imamhossainSajib <ihossainsajib@gmail.com>
2023-05-28 00:38:53 +0600

    Project all init

---
+++ assets/Js/main.js
@@ -0,0 +1,14 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+https://www.youtube.com/watch?v=hmUJBPNMUUI&t=158s
+https://www.youtube.com/watch?v=hmUJBPNMUUI&t=158s
